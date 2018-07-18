0643f3f7070cc6def98384aa04743eaa911493e0
Anand Singh <sanand926@gmail.com>
2018-07-18 19:42:04 +0530

    Added:Header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,3 +1,22 @@
 /**
  * Created by rozer on 7/18/2018.
  */
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Header extends Component{
+    render(){
+        return(
+            <div className="header">
+                <div className="brand">
+                    <Link className="deco" style={{color:"white"}} to="/">MyJewellery</Link>
+                </div>
+                <div className="mProfile">
+                    <Link className="deco" style={{color:"white"}} to="/profile">Anand</Link>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Header
