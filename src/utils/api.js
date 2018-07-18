73dbc8fc20052951dca619717598b9879d747a82
Anand Singh <sanand926@gmail.com>
2018-07-18 19:47:10 +0530

    Added:api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,96 @@
 /**
  * Created by rozer on 7/18/2018.
  */
+
+export const userDetail = {
+    "Name": "Anand Singh",
+    "Age":"20",
+    "Gender":"Male",
+    "Phone":"9041259277",
+    "Email":"sanand926@gmail.com",
+    "Address":"439-A buxi khurda, Daraganj, Allahabad UP, 211006",
+    "uri":require("../dummyImage/preload.jpg"),
+    "Jewellery":[
+        {
+            "id":"r1",
+            "Category":"Ring",
+            "Type":"Light And",
+            "Name":"Light",
+            "uri":require("../dummyImage/ring1.jpg"),
+            "Description":"Fancy Pendant For Girls Women For Party Wear In Gold Plated With Silver Diamond Look Jewellery For Women. Stone-American Diamond Cubic Zirconia With Ad Jewellery With Diamond In Fashion Jewellery And Traditional Imitation Jewellery. Pendant For Girls ,Pendant For Women In Fashion Jewellery."
+        },
+        {
+            "id":"e1",
+            "Category":"Head",
+            "Type":"Earrings",
+            "Name":"Silver Style1",
+            "uri":require("../dummyImage/earring1.jpg"),
+            "Description":"Fancy Pendant For Girls Women For Party Wear In Gold Plated With Silver Diamond Look Jewellery For Women. Stone-American Diamond Cubic Zirconia With Ad Jewellery With Diamond In Fashion Jewellery And Traditional Imitation Jewellery. Pendant For Girls ,Pendant For Women In Fashion Jewellery."
+        },
+        {
+            "id":"r2",
+            "Category":"Ring",
+            "Type":"Dimon",
+            "Name":"Style",
+            "uri":require("../dummyImage/ring2.jpg"),
+            "Description":"Fancy Pendant For Girls Women For Party Wear In Gold Plated With Silver Diamond Look Jewellery For Women. Stone-American Diamond Cubic Zirconia With Ad Jewellery With Diamond In Fashion Jewellery And Traditional Imitation Jewellery. Pendant For Girls ,Pendant For Women In Fashion Jewellery."
+        }
+    ]
+};
+
+export const jewelleries = {
+    "Head":[
+        {
+            "id":"e1",
+            "Category":"Head",
+            "Type":"Earrings",
+            "Name":"Silver Style1",
+            "uri":require("../dummyImage/earring1.jpg"),
+            "Description":"Fancy Pendant For Girls Women For Party Wear In Gold Plated With Silver Diamond Look Jewellery For Women. Stone-American Diamond Cubic Zirconia With Ad Jewellery With Diamond In Fashion Jewellery And Traditional Imitation Jewellery. Pendant For Girls ,Pendant For Women In Fashion Jewellery."
+        },
+        {
+            "id":"e2",
+            "Category":"Head",
+            "Type":"Fascinator",
+            "Name":"Silver Style2",
+            "uri":require("../dummyImage/earring1.jpg"),
+            "Description":"Fancy Pendant For Girls Women For Party Wear In Gold Plated With Silver Diamond Look Jewellery For Women. Stone-American Diamond Cubic Zirconia With Ad Jewellery With Diamond In Fashion Jewellery And Traditional Imitation Jewellery. Pendant For Girls ,Pendant For Women In Fashion Jewellery."
+        }
+    ],
+    "Neck":[
+        {
+            "id":"n1",
+            "Category":"Neck",
+            "Type":"Bolo tie",
+            "Name":"Silver Style",
+            "uri":require("../dummyImage/neckless1.jpg"),
+            "Description":"Fancy Pendant For Girls Women For Party Wear In Gold Plated With Silver Diamond Look Jewellery For Women. Stone-American Diamond Cubic Zirconia With Ad Jewellery With Diamond In Fashion Jewellery And Traditional Imitation Jewellery. Pendant For Girls ,Pendant For Women In Fashion Jewellery."
+        },
+        {
+            "id":"n2",
+            "Category":"Neck",
+            "Type":"Necklace",
+            "Name":"Silver Style",
+            "uri":require("../dummyImage/neckless2.jpg"),
+            "Description":"Fancy Pendant For Girls Women For Party Wear In Gold Plated With Silver Diamond Look Jewellery For Women. Stone-American Diamond Cubic Zirconia With Ad Jewellery With Diamond In Fashion Jewellery And Traditional Imitation Jewellery. Pendant For Girls ,Pendant For Women In Fashion Jewellery."
+        }
+    ],
+    "Ring":[
+        {
+            "id":"r1",
+            "Category":"Ring",
+            "Type":"Light And",
+            "Name":"Light",
+            "uri":require("../dummyImage/ring1.jpg"),
+            "Description":"Fancy Pendant For Girls Women For Party Wear In Gold Plated With Silver Diamond Look Jewellery For Women. Stone-American Diamond Cubic Zirconia With Ad Jewellery With Diamond In Fashion Jewellery And Traditional Imitation Jewellery. Pendant For Girls ,Pendant For Women In Fashion Jewellery."
+        },
+        {
+            "id":"r2",
+            "Category":"Ring",
+            "Type":"Dimon",
+            "Name":"Style",
+            "uri":require("../dummyImage/ring2.jpg"),
+            "Description":"Fancy Pendant For Girls Women For Party Wear In Gold Plated With Silver Diamond Look Jewellery For Women. Stone-American Diamond Cubic Zirconia With Ad Jewellery With Diamond In Fashion Jewellery And Traditional Imitation Jewellery. Pendant For Girls ,Pendant For Women In Fashion Jewellery."
+        }
+    ],
+};
